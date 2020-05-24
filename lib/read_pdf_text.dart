bdde742c3e33f6b8816b12da7f11256dd95903d1
thevikke <31620078+thevikke@users.noreply.github.com>
2020-05-24 13:23:32 +0300

    Update read_pdf_text.dart

--- lib/read_pdf_text.dart
+++ lib/read_pdf_text.dart
@@ -2,19 +2,19 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-// Creating [MethodChannel] to the Android side.
+/// Creating [MethodChannel] to the Android side.
 class ReadPdfText {
   static const MethodChannel _channel = const MethodChannel('read_pdf_text');
 
-  // Returns string, whole text of the document
+  /// Returns string, whole text of the document
   static Future<String> getPDFtext(String path) async {
-    // Mapping the path to <key, value>
+    /// Mapping the path to <key, value>
     final String pdfText = await _channel
         .invokeMethod('getPDFtext', <String, dynamic>{'path': path});
     return pdfText;
   }
 
-  // Returns PDF document as List<String> where each element is a page of the PDF document
+  /// Returns PDF document as List<String> where each element is a page of the PDF document
   static Future<List<String>> getPDFtextPaginated(String path) async {
     // Mapping the path to <key, value>
     List<dynamic> pdfTextPaginated = await _channel
@@ -23,7 +23,7 @@ class ReadPdfText {
     return list;
   }
 
-  // Returns int, number of the pages in the PDF document
+  /// Returns int, number of the pages in the PDF document
   static Future<int> getPDFlength(String path) async {
     // Mapping the path to <key, value>
     final int pdfLength = await _channel
